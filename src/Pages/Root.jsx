9e3faa751d6fed34e9137301fc09d66e2ffec26e
Muhammad Sabbir Khan Alif <muhammadsabbirkhanalif@gmail.com>
2024-07-04 15:54:54 +0100

    Added : footer in the bottom

--- src/Pages/Root.jsx
+++ src/Pages/Root.jsx
@@ -4,11 +4,13 @@ import Navbar from '../Components/Navbar/Navbar'
 import Footer from '../Components/Footer/Footer'
 
 export default function Root() {
-  return (
-    <div className='p-4 font-lato'>
-        <Navbar />
-        <Outlet />
-        <Footer />
-    </div>
-  )
+    return (
+        <div className='p-4 font-lato'>
+            <Navbar />
+            <div className='min-h-[calc(100vh-292px)]'>
+                <Outlet />
+            </div>
+            <Footer />
+        </div>
+    )
 }
